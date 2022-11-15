aff8c2d227d934a528bec9b773c68ea42e2c3513
Hindax <haar162@more2learn.de>
2022-11-15 08:45:24 +0100

    mit While

--- script_01.js
+++ script_01.js
@@ -9,78 +9,124 @@
 */
 
 // Application , App
+// startApp();
+// function startApp() {
+//     output(Calculator(getNumber("Zahl 1"),getNumber("Zahl 2"),getOp()));
+    
+// }
+let isNotAborted; // Globale Variable
 startApp();
 function startApp() {
-    output(Calculator(getNumber("Zahl 1"),getNumber("Zahl 2"),getOp()));
-    
-}
+    let num1,num2,op;
+    isNotAborted = true;
 
+    if (isNotAborted){num1 = getNumber("1st");}
+    if (isNotAborted){num2 = getNumber("2nd");}
+    if (isNotAborted){op = getOp();}
+    
+    if (isNotAborted){
+        output(calculator(num1,num2,op)); 
+    } else{
+        output("Aborted by user!");
+    }
+}
+// function getNumber(figure) {
+//     let inputStr = prompt("Bitte " + figure + " eingeben")
+//     let num = parseInt(inputStr);
+//     return num;
+// }
+
+// module: data input | test:
+//output(getNumber("1st"));
 function getNumber(figure) {
-    let inputStr = prompt("Bitte " + figure + " eingeben")
+    const displayStr = "Please insert " + figure + " number:";
+    let inputStr = prompt(displayStr) 
     let num = parseInt(inputStr);
+
+    // if num is NOT a number AND user DIDN'T click at Abbrechen
+    while (isNaN(num) && (inputStr !== null)) {
+        inputStr =  prompt(displayStr);
+        num = parseInt(inputStr); 
+    }
+    
+    // if this is aborted, ALL gets aborted ...
+    if(inputStr == null){isNotAborted = false;}
     return num;
+
 }
 
 
 
 // module: input operator, test
-output(getOp());
+// output(getOp());
+// function getOp() {
+//     let op = prompt("Please insert an operator");
+//     if (isOpValid(op)) {
+//         return "operator ok";
+//     } else {
+//         return "operator not ok";
+//     }  
+// }
+
+// module: input operator | Test:
+//output(getOp());
+// 
+// module: input operator | Test:
+//output(getOp());
 function getOp() {
-    let op = prompt("Please insert an operator");
-    if (isOpValid(op)) {
-        return "operator ok";
-    } else {
-        return "operator not ok";
-    }  
-}
+    const displayStr = "Please insert correct operator [ + | - | * | : | / ]:";
+    let op = prompt(displayStr); 
 
-function isOpValid(op) {
-
-    switch (op) {
-        case "+":
-        case "-":
-        case "*":
-        case ":":
-        case "/":
-            return true;
-        default:
-            return false;
+    // if op is NOT valid AND user DIDN'T click at Abbrechen
+    while (isNotValidOp(op) && (op !== null)) {
+        op = prompt(displayStr);
     }
-    
+
+    if (op == null){isNotAborted = false;}
+    return op;
 }
+function isNotValidOp(op) {
+    return op != "+" && op != "-" && op != "*" && op != ":" && op != "/";
+}
+// function isOpValid(op) {
+
+//     switch (op) {
+//         case "+":
+//         case "-":
+//         case "*":
+//         case ":":
+//         case "/":
+//             return true;
+//         default:
+//             return false;
+//     }
+    
+// }
 
 
 // module: calculator | tests:
 // agreement : "+","-","*",":"
-// output(calculator(2,2,"+"));
+// output(Calculator(2,2,"+"));
 // output(calculator(2,2,"-"));
 // output(calculator(2,2,"*"));
 // output(calculator(2,2,":"));
 // output(calculator(2,0,"/"));
 // output(calculator(2,2,"#?!"));
 
-
-
-
-function Calculator(a,b,op) {
+function calculator(a,b,op) {
     switch (op) {
         case "+": // addition
             return add(a,b);  
-
         case "-": // substraction
-            return substract(a,b);
-            
+           return substract(a,b);   
         case "*": // multiplikation
             return multiply(a,b);
-
         case ":": // division
         case "/": // division
-            return divide(a,b);
-    
+            return divide(a,b);  
         default: // Error
-            return "Something went wrong!"
+            return "Something went wrong!";
     }
-
 
 }
 
@@ -94,10 +140,9 @@ function divide(a,b) {
     if (b == 0) {
         return "Division by 0 not possible!";
         
-    } else {
-        return a / b;
+    } 
+    return a / b;
     }
-}
     
 
 // module multiplication a * b | test
@@ -140,5 +185,4 @@ function output(outputData) {
         
     }
 }
-
 
